64bd73d332b3b074465264d72238fc1d8931c9d7
Zyr0nX <55247256+Zyr0nX@users.noreply.github.com>
2022-08-04 21:16:18 +0700

    Course and Register api

---
+++ controllers/course.js
@@ -0,0 +1,42 @@
+import { connectMongoose } from "../lib/connect";
+import { Course } from "../models/course";
+
+export const add = async (data) => {
+   
+    await connectMongoose();
+
+    const course = await Course.create(data);
+
+    return course;
+}
+
+export const get = async () => {
+    await connectMongoose();
+
+    const course = await Course.find();
+
+    return course;
+}
+
+export const getOne = async (id) => {
+    await connectMongoose();
+
+    const course = await Course.findOne({ _id: id });
+
+    return course;
+}
+
+export const update = async (id, data) => {
+    await connectMongoose();
+    // Can't update, need fix later
+    const course = await Course.findOneAndUpdate({ _id: id }, { $set: { data } }, { new: true });
+    return course;
+}
+
+export const remove = async (id) => {
+    await connectMongoose();
+
+    const course = await Course.findOneAndDelete({ _id: id });
+
+    return course;
+}

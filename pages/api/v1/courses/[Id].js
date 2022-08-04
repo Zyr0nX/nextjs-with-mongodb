488aa9465c64c9e39d103480472df7cd2477cfb3
Zyr0nX <55247256+Zyr0nX@users.noreply.github.com>
2022-08-04 22:06:18 +0700

    Add course and register api

---
+++ pages/api/v1/courses/[Id].js
@@ -0,0 +1,27 @@
+const courseController = require('../../../../controllers/course');
+
+export default async function handler(req, res) {
+    const id = req.query.Id;
+
+    const method = req.method;
+
+    let result;
+
+    switch (method) {
+        case 'GET':
+            result = await courseController.getOne(id);
+            res.status(200).json(result);
+            break;
+        case 'PUT':
+            result = await courseController.update(id, req.body);
+            res.status(200).json(result);
+            break;
+        case 'DELETE':
+            result = await courseController.remove(id);
+            res.status(200).json(result);
+            break;
+        default:
+            res.status(405).json({ error: 'Method not allowed' });
+            break;
+    }
+}
---
+++ pages/api/v1/registers/[Id].js
@@ -0,0 +1,27 @@
+const registerController = require('../../../../controllers/register');
+
+export default async function handler(req, res) {
+    const id = req.query.Id;
+
+    const method = req.method;
+
+    let result;
+
+    switch (method) {
+        case 'GET':
+            result = await registerController.getOne(id);
+            res.status(200).json(result);
+            break;
+        case 'PUT':
+            result = await registerController.update(id, req.body);
+            res.status(200).json(result);
+            break;
+        case 'DELETE':
+            result = await registerController.remove(id);
+            res.status(200).json(result);
+            break;
+        default:
+            res.status(405).json({ error: 'Method not allowed' });
+            break;
+    }
+}
